ff18fddbfeb4645a2bdb41611158a93a59d95769
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-11-17 19:50:01 -0500

    fixed links and edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Remember - v4.6.0
-* Template URL: https://bootstrapmade.com/remember-free-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
